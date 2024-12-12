09579c15c0e3844c32502955e5cf750c3b1b6d33
Nachthirsch <revengecounter202@gmail.com>
2024-12-12 12:21:11 +0700

    contact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import { AppRoutes } from './routes';
+import { AppRoutes } from "./routes";
 import ScrollToTop from "./components/scrollto-top";
 
 function App() {
